707c10319252779af49802d4fcb74c2e9ff743be
jericovalino15 <jcvalino@multisyscorp.com>
2024-04-19 18:42:56 +0800

    :zap: add table drizzle relations

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,7 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import postgres from 'postgres';
 
-const client = postgres(process.env.SUPABSE_DB_URL as string);
+const client = postgres(process.env.SUPABASE_DB_URL as string, {
+  max: 1,
+});
 export const db = drizzle(client, {
   logger: true,
   schema,
